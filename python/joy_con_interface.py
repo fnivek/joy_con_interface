fc386451a59531e2e81a1bc82bd809367a5b01b8
Kevin French <kdfrench@umich.edu>
2017-08-05 12:38:49 -0500

    BLUETOOTH: Connect to both services provided by each Joy-Con

--- python/joy_con_interface.py
+++ python/joy_con_interface.py
@@ -3,6 +3,8 @@
 import argparse
 import bluetooth
 
+joy_con_names = ['Joy-Con (L)', 'Joy-Con (R)']
+
 
 def parse_cmd_line_args():
     parser = argparse.ArgumentParser(
@@ -11,10 +13,48 @@ def parse_cmd_line_args():
 
 
 def main():
-    print 'Searching for bluetooth devices...'
-    nearby_devices = bluetooth.discover_devices()
-    for addr in nearby_devices:
-        print '\tFound: %s' % (bluetooth.lookup_name(addr),)
+    # Find services
+    print 'Looking for services'
+    services = bluetooth.find_service()
+    joy_cons = []
+    for service in services:
+        print 'Found service: \n\t%s' % (service,)
+        # Lookup the host name to see if its a Joy-Con
+        name = bluetooth.lookup_name(service['host'])
+        print 'The device providing the service is named: %s' % (name)
+        if name in joy_con_names:
+            print 'Found a Joy-Con'
+            joy_cons.append(service)
+
+    # Connect to all Found Joy-Cons
+    print 'Connecting to all Joy-Cons'
+    sockets = []
+    for service in joy_cons:
+        # Get the protocol and start a socket
+        socket = None
+        protocol = service['protocol']
+        if protocol == 'RFCOMM':
+            socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        elif protocol == 'L2CAP':
+            socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+        else:
+            print "Unkown protocol!"
+            continue
+
+        # Connect to the socket on the correct port
+        socket.connect((service['host'], service['port']))
+        sockets.append(socket)
+        print 'Connected'
+
+    # Print all raw data
+    while True:
+        for i, socket in enumerate(sockets):
+            print '-----------------------\nSocket %d' % (i,)
+            print socket.recv(1024)
+
+    # Close all sockets
+    for socket in sockets:
+        socket.close()
 
 
 if __name__ == '__main__':
